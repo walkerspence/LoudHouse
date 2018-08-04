53ac2f62122960baeeefc6be2a4c279ca63ca5be
Walker Spence <walker.spence@gmail.com>
2018-08-03 18:44:07 -0700

    skeleton, basic routes for all pages

---
+++ app/controllers/studio_controller.rb
@@ -0,0 +1,4 @@
+class StudioController < ApplicationController
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  
+  resources :releases
+  resources :artists
+  resources :events
+  resources :studio, only: [:index]
+  resources :about, only: [:index]
+  resources :contact, only: [:index]
+
+  root to: "home#index"
 end
