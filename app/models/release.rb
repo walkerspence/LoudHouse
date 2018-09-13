d8846753e55e8b6646f9ae39f7a1d5350062ba32
Walker Spence <walker.spence@gmail.com>
2018-09-12 18:49:03 -0700

    attempting push to heroku

--- app/models/release.rb
+++ app/models/release.rb
@@ -5,7 +5,7 @@ class Release < ApplicationRecord
                     :styles => { :home => "500x500", :medium => "250x250", :thumb => "100x100>" }
   #TODO set default "missing" image to loudhouse logo
 
-  belongs_to :artist
+  belongs_to :artist, optional: true
 
   validates_attachment :album_art, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
 end
