42b4c8126efcecd525c365948efbce2aa18c5180
Walker Spence <walker.spence@gmail.com>
2018-10-11 13:35:50 -0700

    fixed env var name 2

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -62,4 +62,14 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   config.action_mailer.default_url_options = { host: 'https://loudhouse.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'gmail.com',
+    user_name:            'loudhousepublishing@gmail.com',
+    password:             ENV['EMAIL_PASS'],
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
 end
