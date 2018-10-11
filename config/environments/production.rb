02763121e48eac0b97f3e0209d8af033416a64a2
Walker Spence <walker.spence@gmail.com>
2018-10-11 13:19:27 -0700

    using smtp settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,6 +80,16 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
   config.action_mailer.default_url_options = { :host => 'https://loudhouse.herokuapp.com/' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      address:              'smtp.gmail.com',
+      port:                 587,
+      domain:               'gmail.com',
+      user_name:            'loudhousepublishing@gmail.com',
+      password:             ENV["EMAIL_PASSWORD"],
+      authentication:       'plain',
+      enable_starttls_auto: true
+  }
 
 
   # Use a different logger for distributed setups.
